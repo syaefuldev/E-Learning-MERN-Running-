740b29c4647615a6dab8fdeffa015ef363617c77
syaefulannas45 <syaefulannas33@gmail.com>
2023-02-10 20:35:44 +0700

    update

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors")
 
 const app = express();
 
@@ -17,6 +18,7 @@ const handleErrorMiddleware = require("./app/middlewares/handle-error");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
